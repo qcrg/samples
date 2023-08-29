7fb58c1f79c61be65a08d84aea5a25d550c35a58
qcrg <david-isupov@bk.ru>
2023-08-29 14:14:04 +0000

    add files_installer with check

---
+++ files_installer.py
@@ -0,0 +1,46 @@
+#!/bin/python3
+import os
+
+class FilesInstaller:
+    def __init__(self):
+        self.installed_files = []
+        self.installed_dirs = []
+        self.force = False
+        self.is_bad = False
+
+    def __del__(self):
+        if  self.is_bad:
+            self.restore_installed()
+
+    def restore_installed(self):
+        for file in installed_files:
+            os.unlink(file)
+        for dir_ in reversed(self.installed_dirs):
+            os.rmdir(dir_)
+
+    def install_dir(self, dst):
+        try:
+            os.mkdir(dst)
+            self.installed_dirs.append(dst)
+        except FileExistsError:
+            pass
+
+    def install_file(self, src, dst):
+        if not self.force and os.path.exists(dst):
+            raise Exception("File exists: " + dst)
+        os.rename(src, dst)
+        self.installed_files.append(dst)
+
+    def install(self, src_dir, dst_dir):
+        for (dir_path, dir_names, file_names) in os.walk(src_dir):
+            for dir_name in dir_names:
+                self.install_dir(dst_dir + '/' + dir_name)
+            for file_name in file_names:
+                src = dir_path + '/' + file_name
+                prefix = os.path.commonpath([dir_path, src_dir])
+                dst_prefix = dir_path[len(prefix):]
+                dst = dst_dir + '/' + dst_prefix + '/' + file_name
+                self.install_file(src, dst)
+    
+# installer = FilesInstaller()
+# installer.install('/home/qcrg/foo/cxx', '/home/qcrg/foo/cxx2')
